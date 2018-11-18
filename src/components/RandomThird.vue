e26015aa0aa8bb13af101e494d8513cefab3bea9
flakessp <flakessp@gmail.com>
2018-11-18 14:55:18 +0300

    вариативные свойства

---
+++ src/components/RandomThird.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <h1>вариативные свойства</h1>
+    <div class="container">
+      <section>
+        <h2 :style="computedStyles.letterSpacing" >letter-spacing</h2>
+        <input type="range" name="" id="" min="0" max="30" step="1" v-model="letterSpacing">
+      </section>
+      <section style="text-align: left;">
+        <code>
+          style { <br>
+          &nbsp;letter-spacing: {{letterSpacing}}px<br>
+          }
+        </code>
+      </section>
+    </div>
+    <div class="container">
+      <section>
+        <h2 :style="computedStyles.fontSize" >font-size</h2>
+        <input type="range" name="" id="" min="14" max="50" step="1" v-model="fontSize">
+      </section>
+      <section style="text-align: left;">
+        <code>
+          style { <br>
+          &nbsp;font-size: {{fontSize}}px<br>
+          }
+        </code>
+      </section>      
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        letterSpacing: 0,
+        fontSize: 14
+      }
+    }, computed: {
+      computedStyles: function() {
+        return {
+            letterSpacing: {
+              letterSpacing: this.letterSpacing + 'px'
+            }, fontSize: {
+              fontSize: this.fontSize + 'px'
+            }
+          }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .container {
+    display: flex;
+    justify-content: space-between;
+    padding: 30px;
+  }
+  .container section {
+    border: 1px solid black;
+    padding: 15px;
+    flex-basis: 40%;
+    box-shadow: 5px 10px black;
+    height: fit-content;
+  }
+  code {
+    text-align: left;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import AppHome from '@/components/AppHome'
 import RandomFirst from '@/components/RandomFirst'
 import RandomSecond from '@/components/RandomSecond'
+import RandomThird from '@/components/RandomThird'
 
 Vue.use(Router)
 
@@ -20,6 +21,10 @@ export default new Router({
     {
       path: '/2',
       component: RandomSecond
+    },
+    {
+      path: '/3',
+      component: RandomThird
     }
   ]
 })
